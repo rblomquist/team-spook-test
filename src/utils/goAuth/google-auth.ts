9666a9dbfdecbcb8b4d860d3ccda02f74a6d9614
JulieAntezana <jantezana@byui.edu>
2023-03-22 01:16:56 -0500

    update redirect_uri

--- src/utils/goAuth/google-auth.ts
+++ src/utils/goAuth/google-auth.ts
@@ -6,8 +6,7 @@ import { ParsedQs } from 'qs';
 // Lee los datos de las credenciales de Google desde el archivo JSON
 const credentialsPath = path.join(process.cwd(),process.env.NODE_ENV === "development" ? 'credentials.json':"./credentials.json" );
 const credentials = JSON.parse(readFileSync(credentialsPath, 'utf8'));
-// const REDIRECT_URI ='http://localhost:4000/auth/google/redirect';
-const REDIRECT_URI ='http://tourismagency2023.onrender.com/auth/google/redirect';
+const REDIRECT_URI ='http://localhost:4000/auth/google/redirect';
 const SCOPES = ['https://www.googleapis.com/auth/userinfo.profile'];
 
 // Configuración de OAuth 2.0
